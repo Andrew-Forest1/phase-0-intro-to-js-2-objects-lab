838495316870d5810c8b16826276583d2aeb032d
Andrew <aforest41@yahoo.com>
2022-11-27 12:01:30 -0800

    commit tree 0f927923022142cfcaafd01328f1fefb2a6e9cfd

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name : 'Andrew',
+    address : 'San Jose',
+};
+
+function updateEmployeeWithKeyAndValue(emp, key, value){
+    let emp2 = {...emp};
+    emp2[key] = value;
+    return emp2;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value){
+    emp[key] = value;
+    return emp;
+}
+
+function deleteFromEmployeeByKey(emp, key){
+    let emp2 = {...emp};
+    delete emp2[key];
+    return emp2;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key){
+    delete emp[key];
+    return emp;
+}
